70031a64eba317cfc83376eb8af8eee974ad5293
Layla Oh <crispylemon7120@gmail.com>
2024-07-23 23:25:00 +0900

    Title: 체크판위에서 2, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 240723/체크판위에서 2/on-the-checkboard-2.py
+++ 240723/체크판위에서 2/on-the-checkboard-2.py
@@ -7,12 +7,11 @@ for _ in range(R):
     board.append(sys.stdin.readline().split())
 
 count = 0
-for i in range(1, C - 2):
-    for j in range(1, R - 2):
-        for k in range(i + 1, C - 1):
-            for n in range(j + 1, R - 1):
-                if board[0][0] != board[i][j] and board[i][j] != board[k][n] and board[k][n] != board[C - 1][R - 1]:
+for i in range(1, R - 2):
+    for j in range(1, C - 2):
+        for k in range(i + 1, R - 1):
+            for n in range(j + 1, C - 1):
+                if board[0][0] != board[i][j] and board[i][j] != board[k][n] and board[k][n] != board[R - 1][C - 1]:
                     count += 1
-
 
 print(count)
