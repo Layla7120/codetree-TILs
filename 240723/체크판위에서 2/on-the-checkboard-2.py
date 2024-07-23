c8dfd1273f3fa2ea37cf322a588d8658400f0ecb
Layla Oh <crispylemon7120@gmail.com>
2024-07-23 23:21:02 +0900

    Title: 체크판위에서 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240723/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,18 @@
+import sys
+
+R, C = map(int, sys.stdin.readline().split())
+
+board = []
+for _ in range(R):
+    board.append(sys.stdin.readline().split())
+
+count = 0
+for i in range(1, C - 2):
+    for j in range(1, R - 2):
+        for k in range(i + 1, C - 1):
+            for n in range(j + 1, R - 1):
+                if board[0][0] != board[i][j] and board[i][j] != board[k][n] and board[k][n] != board[C - 1][R - 1]:
+                    count += 1
+
+
+print(count)
