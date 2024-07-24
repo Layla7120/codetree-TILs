766bf6ce1c8c444ca5a1fbe75348788d3bf492ac
Layla Oh <crispylemon7120@gmail.com>
2024-07-24 23:15:57 +0900

    Title: 이상한 진수 2, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240724/이상한 진수 2/awkward-digits-2.py
+++ 240724/이상한 진수 2/awkward-digits-2.py
@@ -2,12 +2,16 @@ import sys
 
 n = sys.stdin.readline()
 
-max_num = int(n, 2)
+max_num = 0
 
 for idx, i in enumerate(n):
     new_n = int(n, 2)
     if i == '0':
         new_n += 2 ** (len(n) - 1 - idx)
+        if new_n > max_num:
+            max_num = new_n
+    else:
+        new_n -= 2 ** (len(n) - 1 - idx)
         if new_n > max_num:
             max_num = new_n
 print(max_num)
