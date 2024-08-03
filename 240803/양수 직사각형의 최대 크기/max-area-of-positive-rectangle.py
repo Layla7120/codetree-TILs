7081ff34403654ea546324cd04e08ebfa2e77a22
Layla Oh <crispylemon7120@gmail.com>
2024-08-03 22:37:13 +0900

    Title: 양수 직사각형의 최대 크기, Time: 154ms, Memory: 26MB, Status: Passed - CodeTree

--- 240803/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240803/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -10,31 +10,30 @@ max_size = 0
 for row in range(n):
     for col in range(m):
         if board[row][col] > 0:
+            # 가로 최대폭 구하기
             width = 1
             for i in range(col + 1, m):
                 if board[row][i] > 0:
                     width += 1
                 else:
                     break
-            height = 1
-            for j in range(row + 1, n):
-                count = 1
-                for k in range(col, col + width):
-                    if board[j][k] > 0:
-                        count += 1
-                    else:
+            # 각각의 가로폭에서 최대 세로폭 구하기 최대 넓이 구하기
+            for w in range(width + 1):
+                height = 1
+                for j in range(row + 1, n):
+                    count = 0
+                    for k in range(col, col + w):
+                        if board[j][k] > 0:
+                            count += 1
+                        else:
+                            break
+                    if count == 0 or count != w:
                         break
-                if count == 0:
-                    break
-                else:
-                    if count < width:
-                        width = count
-                    height += 1
-            final_width = width
-            final_height = height
+                    else:
+                        height += 1
+                if max_size < w * height:
+                    max_size = w * height
 
-            if max_size < final_width * final_height:
-                max_size = final_width * final_height
 if max_size == 0:
     max_size = -1
 
